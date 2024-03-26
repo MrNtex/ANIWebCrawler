85df0afaccfcc8b1f6923143881a208094d0387b
MrNtex <ntexyt@gmail.com>
2024-03-26 19:48:02 +0100

    Add file reading functionality and support for channel identifiers

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use reqwest::Error;
 use serde::Deserialize;
 
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+use std::path::Path;
+
 #[derive(Deserialize, Debug)]
 struct ApiResponse {
     items: Vec<Channel>,
@@ -23,26 +27,43 @@ struct Statistics {
     video_count: Option<String>,
 }
 
+fn read_lines<P>(filename: P) -> io::Result<Vec<String>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    let reader = BufReader::new(file);
+    reader.lines().collect()
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let args: Vec<String> = std::env::args().collect();
-
-    if args.len() < 3 {
-        eprintln!("Usage: {} <channel_id> <api_key>", args[0]);
-        std::process::exit(1);
-    }
-
+    let data = read_lines("channels.txt").unwrap();
+    // if args.len() < 4 {
+    //     eprintln!("Usage: {} <identifier> <api_key> <useId>", args[0]);
+    //     std::process::exit(1);
+    // }
+    let use_id = data[2].eq("true");
     // Adjusted indices for channel_id and api_key
-    fetch_channel_data(&args[1], &args[2]).await?;
+    fetch_channel_data(&data[0], &data[1], use_id).await?;
 
     Ok(())
 }
 
-async fn fetch_channel_data(channel_id: &str, api_key: &str) -> Result<(), Error> {
+async fn fetch_channel_data(identifier: &str, api_key: &str, use_id: bool) -> Result<(), Error> {
+    
+    let (query_param, identifier_value) = if !use_id {
+        ("forUsername", identifier)
+    } else {
+        ("id", identifier)
+    };
+    
     let url = format!(
-        "https://www.googleapis.com/youtube/v3/channels?part=statistics&id={}&key={}",
-        channel_id, api_key
+        "https://www.googleapis.com/youtube/v3/channels?part=statistics&{}={}&key={}",
+        query_param, identifier_value, api_key
     );
+
 
     let client = reqwest::Client::new();
     let res = client.get(&url).send().await?.json::<ApiResponse>().await?;
